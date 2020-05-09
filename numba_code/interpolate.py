28a28c055e863483b7572486858cd56e6c1cb6c9
Lars Mejnertsen <lars.mejnertsen@outlook.com>
2020-05-10 02:46:25 +1000

    Added trilinear interpolation and test cases

--- numba_code/interpolate.py
+++ numba_code/interpolate.py
@@ -35,3 +35,34 @@ def locate_in_grid(xi, x_grid):
             i0 = im+1
 
     return 0
+
+
+@nb.njit('float64(float64, float64, float64, float64[:,:,:])')
+def trilinear_interp(xl, yl, zl, fc):
+    """Tri-Linear interpolation on a given cell.
+
+    Takes in the scaled position within the cell [0 to 1] and cell vertex
+    values and returns the interpolated value. Values outside [0 to 1] are
+    extrapolated.
+
+    Args:
+        xl (float): scaled x position [0 to 1]
+        yl (float): scaled y position [0 to 1]
+        zl (float): scaled z position [0 to 1]
+        fc (np.array(float)): cell values as the vertex
+
+    Returns:
+        float: interpolated value
+    """
+    m_xl = 1 - xl
+    m_yl = 1 - yl
+
+    c00 = fc[0, 0, 0]*m_xl + fc[1, 0, 0]*xl
+    c01 = fc[0, 0, 1]*m_xl + fc[1, 0, 1]*xl
+    c10 = fc[0, 1, 0]*m_xl + fc[1, 1, 0]*xl
+    c11 = fc[0, 1, 1]*m_xl + fc[1, 1, 1]*xl
+
+    c0 = c00*m_yl + c10*yl
+    c1 = c01*m_yl + c11*yl
+
+    return c0*(1 - zl) + c1*zl
