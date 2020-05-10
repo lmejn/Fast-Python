55c722b59a7d5b5bb356c34f156b5b4f6d97f125
Lars Mejnertsen <lars.mejnertsen@outlook.com>
2020-05-10 15:50:04 +1000

    Added streamline calc for fortran to main.py
    - main.py calculates and plots streamlines from both methods
    - Also calculates the distance between the two streamlines, which should
      be almost zero

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import numba_code.streamtracer as nb_streamtracer
+from fortran import streamtracer as f_stream
+from numba_code import streamtracer as n_stream
 from grid import generate_grid
 from plot import plot_streamline_3D, plot_streamline
 
 if __name__ == "__main__":
+
+    # np.random.seed(23849234)
 
     xc, yc, zc, vx, vy, vz = generate_grid(200)
 
@@ -13,23 +16,39 @@ if __name__ == "__main__":
 
     # Calculate Streamline
 
-    ns = int(1e4)
+    ns = int(1e6)
     step_size = 0.1
-    ds = np.zeros(ns)
 
-    xs, n_steps = nb_streamtracer.calc_streamline(xs0,
-                                                  xc, yc, zc, vx, vy, vz,
-                                                  ds, step_size)
-    xs = xs[:n_steps]
-    ds = ds[:n_steps]
-    s = ds.cumsum()
+    ds_f = np.zeros(ns)
+
+    xs_f, ns_f = f_stream.calc_streamline(xs0, xc, yc, zc, vx, vy, vz,
+                                           ds_f, step_size)
+    xs_f = xs_f[:ns_f]
+    ds_f = ds_f[:ns_f]
+    s_f = ds_f.cumsum()
+    
+    ds_n = np.zeros(ns)
+
+    xs_n, ns_n = n_stream.calc_streamline(xs0, xc, yc, zc, vx, vy, vz,
+                                           ds_n, step_size)
+    xs_n = xs_n[:ns_n]
+    ds_n = ds_n[:ns_n]
+    s_n = ds_n.cumsum()
 
     # Plot Streamline
     fig = plt.figure(figsize=(8, 8))
-    ax = plot_streamline_3D(xs)
+    ax = plot_streamline_3D(xs_f)
+    plot_streamline_3D(xs_n, ax=ax)
+
     ax.set(xlim=xc[[0, -1]], ylim=yc[[0, -1]], zlim=zc[[0, -1]])
 
     fig, ax = plt.subplots()
-    ax = plot_streamline(s, xs, ax=ax)
+    plot_streamline(s_f, xs_f, ax=ax)
+    ax.set_prop_cycle(None)
+    plot_streamline(s_n, xs_n, ax=ax, linestyle='dashed', linewidth=2)
+
+    fig, ax = plt.subplots()
+    error = np.sqrt(np.sum((xs_f - xs_n)**2, axis=-1))
+    ax.plot(s_n, error)
 
     plt.show()
