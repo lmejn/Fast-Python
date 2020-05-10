ad4206a5cf598cc7387941f16452fb345423946d
Lars Mejnertsen <lars.mejnertsen@outlook.com>
2020-05-10 13:47:04 +1000

    Refactored grid generation code
    - All code moved to grid.py
    - Gridded positions and velocity is return using a single generate_grid

--- main.py
+++ main.py
@@ -1,70 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import numba_code.streamtracer as nb_streamtracer
-
-
-def poly3D(x, y, z, coeff):
-    """Sample a polynomial 3D scalar field
-
-    Polynomial order is specified by the number of elements in each row in
-    coeff. Number of rows should always ==3. e.g. polynomial of order 2
-    requires coeff of shape [3, 2].
-
-    Args:
-        x (float): x position
-        y (float): y position
-        z (float): z position
-        coeff (np.array): Coefficients of the polynomial scalar field
-    
-    Returns:
-        value of scalar field
-    """       
-    fx = np.polyval(coeff[0], x)
-    fy = np.polyval(coeff[1], y)
-    fz = np.polyval(coeff[2], z)
-
-    return fx*fy*fz
-
-
-def create_grid_axis(n, lim):
-    """Create a uniformly spaced axis.
-
-    Args:
-        n (int): number of cells
-        lim (list): list containing lower and upper bound
-    
-    Returns:
-        xb: np.array containing cell boundary positions
-        xc: np.array containing cell centre positions
-        dx: np.array containing cell sizes
-    """
-    xb = np.linspace(lim[0], lim[1], n+1)
-    xc = 0.5*(xb[1:]+xb[:-1])
-    dx = np.diff(xb)
-
-    return xb, xc, dx
-
+from grid import generate_grid
 
 if __name__ == "__main__":
 
-    # Generate grid
-    n = np.array([1, 1, 1])*200
-
-    xb, xc, dx = create_grid_axis(n[0], [-1, 1])
-    yb, yc, dy = create_grid_axis(n[1], [-1, 1])
-    zb, zc, dz = create_grid_axis(n[2], [-1, 1])
-
-    Yc, Xc, Zc = np.meshgrid(yc, xc, zc)
-
-    # Store polynomial coefficients
-    cx = 2*np.random.rand(3, 3)-1
-    cy = 2*np.random.rand(3, 3)-1
-    cz = 2*np.random.rand(3, 3)-1
-
-    # Calculate vector field
-    vx = poly3D(Xc, Yc, Zc, cx)
-    vy = poly3D(Xc, Yc, Zc, cy)
-    vz = poly3D(Xc, Yc, Zc, cz)
+    xc, yc, zc, vx, vy, vz = generate_grid(200)
 
     # Set Seed Point
     xs0 = np.array([0., 0., 0.])
@@ -87,6 +28,6 @@ if __name__ == "__main__":
     p = ax.plot(xs[:, 0], xs[:, 1], xs[:, 2])
     ax.plot(xs[[0], 0], xs[[0], 1], xs[[0], 2], '.', color=p[0].get_color())
     ax.plot(xs[[-1], 0], xs[[-1], 1], xs[[-1], 2], 'x', color=p[0].get_color())
-    ax.set(xlim=xb[[0, -1]], ylim=yb[[0, -1]], zlim=zb[[0, -1]],
+    ax.set(xlim=xc[[0, -1]], ylim=yc[[0, -1]], zlim=zc[[0, -1]],
            xlabel='x', ylabel='y', zlabel='z')
     plt.show()
